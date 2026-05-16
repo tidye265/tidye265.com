d5f98a8ba1daf5c03032135bafec5cf2f6e87973
tidye265 <moskonx@gmail.com>
2026-05-16 09:17:31 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://egepxfikpdmpruilzism.supabase.co";
-
-const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZXB4ZmlrcGRtcHJ1aWx6aXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDY1NzgsImV4cCI6MjA5NDM4MjU3OH0.Jf3lQyKMXntcvrdsItEN-mPPTnQFCpJEGikO0cjInEc";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
