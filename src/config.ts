f417944091392dae304982b0a45825f89d6506ea
tidye265 <moskonx@gmail.com>
2026-06-08 00:01:44 +0200

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  development: false,
+  debug: true,
+  appKey: "crash-0.1.0",
+  api: `${process.env.REACT_APP_API_URL}/api`,
+  // Sinthani mzerewu kukhala URL ya server yanu ya Hugging Face
+  wss: "https://moskonx-tidye-shooter-game-server.hf.space", 
+};
