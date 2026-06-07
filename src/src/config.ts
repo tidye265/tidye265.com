893da1d8176e3793db77227ed394d371c92429a4
tidye265 <moskonx@gmail.com>
2026-06-08 00:01:08 +0200

    Delete src/src directory

--- src/src/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  development: false,
-  debug: true,
-  appKey: "crash-0.1.0",
-  api: `${process.env.REACT_APP_API_URL}/api`,
-  // Sinthani mzerewu kukhala URL ya server yanu ya Hugging Face
-  wss: "https://moskonx-tidye-shooter-game-server.hf.space", 
-};
