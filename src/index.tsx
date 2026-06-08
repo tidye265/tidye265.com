eab3cf647d6d5553b43641ffbba5a8a4b0ee737a
tidye265 <moskonx@gmail.com>
2026-06-08 11:57:56 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+// index.tsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom'; // Chotsani Routes ndi Route
+
+import './index.scss';
+import App from './app';
+import { Provider } from './context';
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+      <ToastContainer position="top-center" theme="dark" />
+    </Provider>
+  </BrowserRouter>
+);
