0dbb33d92feb1d37be3fa017a463cb83ccb4316f
tidye265 <moskonx@gmail.com>
2026-08-01 02:25:00 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+/* src/main.jsx */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Kukhazikitsa Bootstrap Icons
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
