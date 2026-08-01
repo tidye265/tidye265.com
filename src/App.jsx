65b51b8656126f24cb36cf754d8e7a5e3ebe7bab
tidye265 <moskonx@gmail.com>
2026-08-01 13:29:45 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* src/App.jsx */
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        fontSize: "40px",
+        color: "red",
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: "100px",
+      }}
+    >
+      TEST APP 123
+    </div>
+  );
+}
